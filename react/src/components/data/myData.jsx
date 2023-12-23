7f8293b8181282874778b094e4544f58f193d933
tommys0 <149568540+tommys0@users.noreply.github.com>
2023-12-23 23:21:00 +0100

    New data

--- react/src/components/data/myData.jsx
+++ react/src/components/data/myData.jsx
@@ -1,139 +1,132 @@
 const data = [
     {
-        "name": "Course 1",
+        "name": "Introduction to HTML",
         "speaker": "Dr. Smith",
         "location": "Los Angeles",
         "date": "2023-03-21",
         "isNew": true
     },
     {
-        "name": "Course 2",
+        "name": "CSS Fundamentals",
         "speaker": "Prof. Johnson",
         "location": "Los Angeles",
         "date": "2023-08-26",
         "isNew": true
     },
     {
-        "name": "Course 3",
+        "name": "JavaScript Basics",
         "speaker": "Dr. Brown",
         "location": "Chicago",
         "date": "2023-08-23",
         "isNew": true
     },
     {
-        "name": "Course 4",
+        "name": "Java Programming Masterclass",
         "speaker": "Dr. Williams",
         "location": "New York",
         "date": "2023-11-03",
         "isNew": true
     },
     {
-        "name": "Course 5",
-        "speaker": "Prof. Davis",
-        "location": "Houston",
-        "date": "2023-11-05",
-        "isNew": false
-    },
-    {
-        "name": "Course 6",
+        "name": "Web Development Bootcamp",
         "speaker": "Prof. Davis",
         "location": "New York",
         "date": "2023-11-10",
         "isNew": true
     },
     {
-        "name": "Course 7",
+        "name": "Advanced JavaScript Techniques",
         "speaker": "Dr. Williams",
         "location": "New York",
         "date": "2023-09-22",
         "isNew": true
     },
     {
-        "name": "Course 8",
+        "name": "Python for Data Science",
         "speaker": "Prof. Martinez",
         "location": "San Francisco",
         "date": "2023-04-15",
         "isNew": false
     },
     {
-        "name": "Course 9",
+        "name": "Cybersecurity Fundamentals",
         "speaker": "Dr. Thompson",
         "location": "Miami",
         "date": "2023-06-18",
         "isNew": true
     },
     {
-        "name": "Course 10",
+        "name": "ReactJS Workshop",
         "speaker": "Prof. Lee",
         "location": "Seattle",
         "date": "2023-07-09",
         "isNew": true
     },
     {
-        "name": "Course 11",
+        "name": "Android App Development",
         "speaker": "Dr. Garcia",
         "location": "Dallas",
         "date": "2023-09-30",
         "isNew": false
     },
     {
-        "name": "Course 12",
+        "name": "Spring Framework Essentials",
         "speaker": "Prof. Rodriguez",
         "location": "Boston",
         "date": "2023-10-12",
         "isNew": true
     },
     {
-        "name": "Course 13",
+        "name": "Machine Learning Fundamentals",
         "speaker": "Dr. Nguyen",
         "location": "Atlanta",
         "date": "2023-11-18",
         "isNew": true
     },
     {
-        "name": "Course 14",
+        "name": "Node.js Mastery",
         "speaker": "Prof. Wilson",
         "location": "Las Vegas",
         "date": "2023-12-05",
         "isNew": false
     },
     {
-        "name": "Course 15",
+        "name": "AWS Cloud Practitioner",
         "speaker": "Dr. Martinez",
         "location": "Phoenix",
         "date": "2023-05-30",
         "isNew": true
     },
     {
-        "name": "Course 16",
+        "name": "Data Structures in C++",
         "speaker": "Prof. Taylor",
         "location": "Denver",
         "date": "2023-08-08",
         "isNew": true
     },
     {
-        "name": "Course 17",
+        "name": "DevOps Essentials",
         "speaker": "Dr. Harris",
         "location": "Philadelphia",
         "date": "2023-09-12",
         "isNew": false
     },
     {
-        "name": "Course 18",
+        "name": "Web Design Principles",
         "speaker": "Prof. Lewis",
         "location": "Orlando",
         "date": "2023-10-25",
         "isNew": true
     },
     {
-        "name": "Course 19",
+        "name": "Java Enterprise Applications",
         "speaker": "Dr. Clark",
         "location": "Portland",
         "date": "2023-11-28",
         "isNew": true
     },
     {
-        "name": "Course 20",
+        "name": "Full Stack Development",
         "speaker": "Prof. Allen",
         "location": "Austin",
         "date": "2023-12-15",
